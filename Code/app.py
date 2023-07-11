ddda3e8bf568f7e452a0a92002d1897422380b99
Louisa Goncharenko <louisa.gonch@gmail.com>
2023-07-11 14:57:15 -0400

    saving progress

--- Code/app.py
+++ Code/app.py
@@ -14,7 +14,7 @@ def home():
 
 
 if __name__ == "__main__":
-    """To run the Flask server, execute `python app.py` in your terminal.
+    """To run the Flask server, execute `python3 app.py` in your terminal.
        To learn more about Flask's DEBUG mode, visit
        https://flask.palletsprojects.com/en/2.0.x/server/#in-code"""
     app.run(debug=True)
